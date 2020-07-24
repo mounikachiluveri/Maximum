1ae709d6eec2cba391159531915de5218b6f1fe2
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-25 00:03:27 +0530

    [Mounika] Added:Code Refactered changed to generic type

--- src/main/java/MaximumNumber.java
+++ src/main/java/MaximumNumber.java
@@ -1,5 +1,14 @@
-public class MaximumNumber {
-    public static Integer testMaximum(Integer X, Integer Y, Integer Z) {
+public class MaximumNumber<E extends Comparable> {
+    E X;
+    E Y;
+    E Z;
+
+    public MaximumNumber(E X, E Y, E Z) {
+        this.X = X;
+        this.Y = Y;
+        this.Z = Z;
+    }
+    /* public static Integer testMaximum(Integer X, Integer Y, Integer Z) {
         Integer max = X;
         if (Y.compareTo(max) > 0) {
             max = Y;
@@ -9,6 +18,7 @@ public class MaximumNumber {
         }
         return max;
     }
+
     public static Float testMaximum(Float X, Float Y, Float Z) {
         Float max = X;
         if (Y.compareTo(max) > 0) {
@@ -19,6 +29,7 @@ public class MaximumNumber {
         }
         return max;
     }
+
     public static String testMaximum(String X, String Y, String Z) {
         String max = X;
         if (Y.compareTo(max) > 0) {
@@ -28,7 +39,24 @@ public class MaximumNumber {
             max = Z;
         }
         return max;
+    }*/
+
+    public static <E extends Comparable<E>> E testMaximum(E X, E Y, E Z) {
+        E max = X;
+        if (Y.compareTo(max) > 0) {
+            max = Y;
+        }
+        if (Z.compareTo(max) > 0) {
+            max = Z;
+        }
+        return max;
     }
+
+    public <E extends Comparable<E>> E testMaximum() {
+        E max = (E) testMaximum(X, Y, Z);
+        return max;
+    }
+
 }
 
 
--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -4,64 +4,45 @@ import org.junit.Test;
 public class MaximumNumberTest {
     @Test
     public void givenIntegerAtFirstNumberMax_ShouldReturnTrue() {
-        MaximumNumber maximumNumber = new MaximumNumber();
-        int maximum = maximumNumber.testMaximum(92, 12, 34);
-        Assert.assertEquals(92, maximum);
+        Assert.assertEquals(92,new MaximumNumber(92,12,34).testMaximum());
     }
 
     @Test
     public void givenIntegerAtSecondNumberMax_ShouldReturnTrue() {
-        MaximumNumber maximumNumber = new MaximumNumber();
-        int maximum = maximumNumber.testMaximum(12, 95, 34);
-        Assert.assertEquals(95, maximum);
+        Assert.assertEquals(95,new MaximumNumber(45,95,34).testMaximum());
     }
 
     @Test
     public void givenIntegerAtThirdNumberMax_ShouldReturnTrue() {
-        MaximumNumber maximumNumber = new MaximumNumber();
-        int maximum = maximumNumber.testMaximum(12, 34, 87);
-        Assert.assertEquals(87, maximum);
+        Assert.assertEquals(92,new MaximumNumber(45,34,92).testMaximum());
     }
 
     @Test
     public void givenFloatAtFirstNumberMax_ShouldReturnTrue() {
-        MaximumNumber maximumNumber = new MaximumNumber();
-        float maximum = maximumNumber.testMaximum(12.0f, 11.0f, 10.0f);
-        Assert.assertEquals(12.0f, maximum, 0.0);
+        Assert.assertEquals(12.0f, new MaximumNumber(12.0f, 11.0f, 10.0f).testMaximum());
     }
 
     @Test
     public void givenFloatAtSecondNumberMax_ShouldReturnTrue() {
-        MaximumNumber maximumNumber = new MaximumNumber();
-        float maximum = maximumNumber.testMaximum(11.0f, 12.0f, 10.0f);
-        Assert.assertEquals(12.0f, maximum, 0.0);
+        Assert.assertEquals(12.0f, new MaximumNumber(11.0f, 12.0f, 10.0f).testMaximum());
     }
 
     @Test
     public void givenFloatAtThirdNumberMax_ShouldReturnTrue() {
-        MaximumNumber maximumNumber = new MaximumNumber();
-        float maximum = maximumNumber.testMaximum(11.0f, 10.0f, 12.0f);
-        Assert.assertEquals(12.0f, maximum, 0.0);
+        Assert.assertEquals(12.0f, new MaximumNumber(10.0f, 11.0f, 12.0f).testMaximum());
     }
 
     @Test
     public void givenStringAtFirstPostionMax_ShouldReturnTrue() {
-        MaximumNumber maximumNumber = new MaximumNumber();
-        String maximum = maximumNumber.testMaximum("Peach", "Apple", "Banana");
-        Assert.assertEquals("Peach", maximum);
+        Assert.assertEquals("Peach",new MaximumNumber("Peach", "Apple", "Banana").testMaximum());
     }
 
     @Test
     public void givenStringAtSecondPositionMax_ShouldReturnTrue() {
-        MaximumNumber maximumNumber = new MaximumNumber();
-        String maximum = maximumNumber.testMaximum("Apple", "Peach", "Banana");
-        Assert.assertEquals("Peach", maximum);
+        Assert.assertEquals("Peach",new MaximumNumber("Apple", "Peach", "Banana").testMaximum());
     }
-
     @Test
     public void givenStringAtThirdPositionMax_ShouldReturnTrue() {
-        MaximumNumber maximumNumber = new MaximumNumber();
-        String maximum = maximumNumber.testMaximum("Apple", "Banana", "Peach");
-        Assert.assertEquals("Peach", maximum);
+        Assert.assertEquals("Peach",new MaximumNumber("Banana", "Apple", "Peach").testMaximum());
     }
 }
